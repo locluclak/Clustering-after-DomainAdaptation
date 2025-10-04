Fix: the sign in test statistic

si_cluster_da_checkunif_multidim.py
@@ -42,19 +42,14 @@ final_model.load_model("trained_model/20250927-154452-20dims")
 
 
 
-def test_statistic(X, X_vec, Xt, ns, nt, d, n_clusters, Sigma, labels_all_obs, members_all_obs):
+def test_statistic(X_vec, Xt, ns, nt, d, n_clusters, Sigma, labels_all_obs, members_all_obs,return_sign=False):
 
     c1, c2 = np.random.choice(n_clusters, 2, replace=False)
     idx_cluster_c1 = np.argwhere(labels_all_obs[-1][ns:] == c1).flatten()
     idx_cluster_c2 = np.argwhere(labels_all_obs[-1][ns:] == c2).flatten()
     if idx_cluster_c1.size == 0 or idx_cluster_c2.size == 0:
         return None
-   
-            
-    # cluster_c1_obs = members_all_obs[-1][c1]
-    # cluster_c2_obs = members_all_obs[-1][c2]
 
-    
     I_d = np.identity(d)
     
     eta_c1_idx = np.zeros((nt, 1))
@@ -67,17 +62,14 @@ def test_statistic(X, X_vec, Xt, ns, nt, d, n_clusters, Sigma, labels_all_obs, m
     eta_c2 = np.kron(I_d, eta_c2_idx)
    
     eta_tmp = eta_c1 - eta_c2
-    # print(eta_tmp)
-    
-    sign_tmp = np.dot(eta_c1_idx.T, Xt) - np.dot(eta_c2_idx.T, Xt)
-    # print(sign_tmp)
-    sign = np.transpose(np.sign(sign_tmp))
-    # print(sign)
+    sign_tmp = np.dot(eta_tmp.T, vec(Xt))
+    sign = sign_tmp
+    if return_sign:
+        return sign
     eta_sign = np.dot(eta_tmp, sign)
 
     eta = np.vstack((np.zeros((ns*d, 1)), eta_sign))
     etaTXvec = np.dot(eta.T, X_vec)
-    # print(etaTXvec)
 
     etaT_Sigma_eta = np.dot(np.dot(eta.T, Sigma), eta)
     b = np.dot(np.dot(Sigma, eta), np.linalg.inv(etaT_Sigma_eta))
@@ -97,38 +89,19 @@ def test_statistic(X, X_vec, Xt, ns, nt, d, n_clusters, Sigma, labels_all_obs, m
     }
     
 def overconditioning(model, X, a, b, np_wdgrl, n_clusters, initial_centroids_obs, labels_all_obs, members_all_obs,z=0,X_=None):
-    # st = time.time()
-    # print("a+b*z - X",np.sum(a+b*z - X))
     if device == "cpu":
         interval_da, a_, b_ = construct_interval.ReLUcondition(model.encoder, a, b, X)
     else:        
         interval_da, a_, b_ = conditioning.get_dnn_interval(X,a,b,np_wdgrl)
         interval_da = [interval_da]
-    # print("a_+b_*z - X_",np.sum(a_+b_*z - X_))
 
-
-
-    # st1 = time.time()
     # interval_kmean = construct_interval.KMeancondition(X.shape[0], n_clusters, a_, b_, initial_centroids_obs, labels_all_obs, members_all_obs,z)
     interval_kmean = construct_interval.KMeancondition2(X.shape[0], n_clusters, a_, b_, initial_centroids_obs, labels_all_obs, members_all_obs,z)
-    # st15 = time.time()
     # interval_kmean = construct_interval.KMeanconditionCUPY(X.shape[0], n_clusters, a_, b_, initial_centroids_obs, labels_all_obs, members_all_obs,z)
-    # st16 = time.time()
-    # interval_kmean = construct_interval.KMeancondition3(X.shape[0], n_clusters, a_, b_, initial_centroids_obs, labels_all_obs, members_all_obs,z)
-    # p, q, o = construct_interval.KMeancondition2(X.shape[0], n_clusters, a_, b_, initial_centroids_obs, labels_all_obs, members_all_obs)
-    # st17 = time.time()
-    # print("kmean2",st15-st1)
-    # print("kmean3",st16-st15)
-    # interval_kmean2 = construct_interval.solveinterval(p,q,o)
-    # st2 = time.time()
 
-    # print("Interval by DA:", interval_da)
-    # print(f"Time constructing interval by DA: {st1 - st:.4f} seconds")
-    # print(f"Time constructing interval by Kmean: {st15 - st1:.4f} seconds")
-    # print(f"Time constructing interval by Kmean2: {st17 - st15:.4f} seconds")
     final_interval = util.interval_intersection(interval_da, interval_kmean)
     return final_interval
-def parametric(model, X, a, b,np_wdgrl, n_clusters, c1, c2, c1_obs, c2_obs, zmin = -20, zmax = 20, log=None, seed=None):
+def parametric(model, X, a, b,np_wdgrl, n_clusters, c1, c2, c1_obs, c2_obs, signobs, zmin = -20, zmax = 20, log=None, seed=None):
     global device
     n, d = X.shape
     z =  zmin
@@ -148,13 +121,13 @@ def parametric(model, X, a, b,np_wdgrl, n_clusters, c1, c2, c1_obs, c2_obs, zmin
                 # Xdeltaz_transformed = final_model.extract_feature(Xdeltaz_torch).cpu().numpy()
                 Xdeltaz_transformed = model.extract_feature(Xdeltaz_torch).cpu().numpy()
             initial_centroids_obs, labels_all_obs, members_all_obs = kmeans(Xdeltaz_transformed, n_clusters)
-            
+            sign_z = test_statistic(vec(Xdeltaz),Xdeltaz[ns:], ns, nt, d, n_clusters, Sigma=None, labels_all_obs=labels_all_obs, members_all_obs=members_all_obs,return_sign=True )
             # oc = util.interval_intersection(intervalFS,intervalDA)
             oc = overconditioning(model, Xdeltaz, a, b, np_wdgrl, n_clusters, initial_centroids_obs, labels_all_obs, members_all_obs, z=z,X_=Xdeltaz_transformed)
             idx_cluster_c1 = np.argwhere(labels_all_obs[-1][ns:] == c1).flatten()
             idx_cluster_c2 = np.argwhere(labels_all_obs[-1][ns:] == c2).flatten()
 
-            if np.array_equal(c1_obs, idx_cluster_c1) and np.array_equal(c2_obs, idx_cluster_c2):
+            if np.array_equal(c1_obs, idx_cluster_c1) and np.array_equal(c2_obs, idx_cluster_c2) and np.array_equal(signobs, sign_z):
                 Z = util.interval_union(Z, oc)
                 countitv+=1
             # print("oc:", oc)
@@ -171,12 +144,15 @@ def parametric(model, X, a, b,np_wdgrl, n_clusters, c1, c2, c1_obs, c2_obs, zmin
             f.write(f"Final interval: {Z}\n")
     # print("Final interval:", Z)
     return Z
+
 def vec(A):
     vec = A.reshape(-1)
     return vec.reshape(-1,1)
+
+
 def run(mu_s, mu_t, K, device,_=None):
     global final_model
-    dataseed = None #random.randint(0, 2**32 - 1)  # 32-bit seed
+    dataseed = None
     # print("Data seed:", dataseed)
     # ---- Generate synthetic data ----
     # try:
@@ -197,7 +173,7 @@ def run(mu_s, mu_t, K, device,_=None):
     
     Xs_vec = vec(Xs)
     Xt_vec = vec(Xt)
-    X_vec = np.vstack((Xs_vec, Xt_vec)).copy()
+    X_vec = np.vstack((Xs_vec, Xt_vec))
     X_origin = np.vstack((Xs, Xt))
     X_transformed = np.vstack((xs_hat, xt_hat))
 
@@ -205,26 +181,26 @@ def run(mu_s, mu_t, K, device,_=None):
     # print(labels_all_obs)
     Sigma = np.identity(n*d)
     try:
-        a, b, etaTX, etaT_Sigma_eta, c1, c2, c1_obs, c2_obs, sign = test_statistic(X_origin, X_vec, Xt, ns, nt, d, K, Sigma, labels_all_obs, members_all_obs).values()
-    except:
-        print("test statistic is none") 
+        a, b, etaTX, etaT_Sigma_eta, c1, c2, c1_obs, c2_obs, sign = test_statistic(X_vec, Xt, ns, nt, d, K, Sigma, labels_all_obs, members_all_obs).values()
+    except Exception as e:
+        print("test statistic is none", e) 
         return None
     a_2d = a.reshape(n, d)
     b_2d = b.reshape(n, d)
-    # with torch.no_grad():
-    #     a_hat = final_model.extract_feature(torch.from_numpy(a_2d).double().to(device)).cpu().numpy()
-    #     b_hat = final_model.extract_feature(torch.from_numpy(b_2d).double().to(device)).cpu().numpy()
-    # print("run", np.sum(a_hat + b_hat*etaTX - X_transformed))
+
     np_wdgrl = None# operations.convert_network_to_numpy(final_model.encoder)
     # final_model.encoder = final_model.encoder.to(device)
+
+
+
+
     final_interval = overconditioning(final_model, X_origin, a_2d, b_2d,np_wdgrl, K, initial_centroids_obs, labels_all_obs, members_all_obs,z=etaTX, X_=X_transformed)
-    # st = time.time()
-    # final_interval = parametric(final_model, X_origin, a_2d, b_2d,np_wdgrl, K, c1, c2, c1_obs, c2_obs, zmin=-20, zmax=20,seed=None)
-    # en = time.time()
+    # final_interval = parametric(final_model, X_origin, a_2d, b_2d,np_wdgrl, K, c1, c2, c1_obs, c2_obs, signobs = sign, zmin=-20, zmax=20,seed=None)
     # final_interval = [(-np.inf, np.inf)]
-    # print(f"Time for parametric: {en-st:.4f} seconds")
+    
+    
     selective_p_value = util.compute_p_value(final_interval, etaTX, etaT_Sigma_eta)
-    print(f"p-value:", selective_p_value)
+    print(f"test-stat: {etaTX}, p-value:", selective_p_value)
 
     # with open(f'logs/selective_inference_log/FPRnaivep_values{ns}.txt', 'a') as f:
     #     f.write(f"{selective_p_value}\n")